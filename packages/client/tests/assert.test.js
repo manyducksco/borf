c9e34a28ccba135c94727c3d03df19930b75f16d
Tony McCoy <tony@ratwizard.io>
2021-11-13 21:24:37 -0800

    Added assert util for type checking

---
+++ packages/client/tests/assert.test.js
@@ -0,0 +1,14 @@
+import { assert } from "../dist/woof";
+
+test("asserts number types", () => {
+  expect(() => assert(5).number().integer().min(2).max(4)).toThrowError(
+    /Expected a number/
+  );
+});
+
+test("asserts string types", () => {
+  expect(() => assert(5).string()).toThrow();
+  expect(() => assert("5").string().length(2)).toThrowError(
+    /Expected a 2 character string/
+  );
+});
