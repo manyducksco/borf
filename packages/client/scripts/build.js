feebd8225a33a0c9cead39cb537a0884e2180971
Tony McCoy <tony@ratwizard.io>
2021-11-22 11:46:09 -0800

    Simplified build scripts

--- packages/client/scripts/build.js
+++ packages/client/scripts/build.js
@@ -1,9 +1,46 @@
-require("esbuild")
+const esbuild = require("esbuild");
+
+esbuild
   .build({
-    entryPoints: ["src/main.ts"],
+    entryPoints: ["src/index.js"],
     bundle: true,
     minify: true,
     sourcemap: true,
-    outfile: "dist/main.js",
+    platform: "browser",
+    format: "esm",
+    outfile: "dist/woof.js",
+  })
+  .then(() => {
+    console.log("Created ESM bundle");
+  })
+  .catch(() => process.exit(1));
+
+esbuild
+  .build({
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    platform: "node",
+    format: "cjs",
+    outfile: "dist/woof.node.js",
+  })
+  .then(() => {
+    console.log("Created node bundle");
+  })
+  .catch(() => process.exit(1));
+
+esbuild
+  .build({
+    entryPoints: ["src/testing/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    platform: "browser",
+    format: "esm",
+    outfile: "dist/woof.testing.js",
+  })
+  .then(() => {
+    console.log("Created testing bundle");
   })
   .catch(() => process.exit(1));
