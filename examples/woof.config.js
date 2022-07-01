1d951ecb74d5c7bfb68415bac3036c13078f94ad
Tony McCoy <tony@ratwizard.io>
2022-06-30 21:10:17 -0700

    Support woof config as both CJS and ES for build

--- examples/woof.config.js
+++ examples/woof.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   // Include global project CSS in views
   view: {
     include: {
--- packages/build/utils/findWoofConfig.js
+++ packages/build/utils/findWoofConfig.js
@@ -5,12 +5,18 @@ const process = require("process");
 /**
  * Finds the nearest woof.config.js file in parent directories. This should be at the root of your project if one exists.
  */
-module.exports = function findWoofConfig(dir = process.cwd()) {
+module.exports = async function findWoofConfig(dir = process.cwd()) {
   const files = fs.readdirSync(dir);
 
   for (const file of files) {
     if (/^woof\.config\.[cm]?js$/.test(file)) {
-      return require(path.join(dir, file));
+      const module = await import(path.join(dir, file));
+
+      if (module.default) {
+        return module.default;
+      }
+
+      return module;
     }
   }
 
