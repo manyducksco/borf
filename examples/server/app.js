13802dd379ac730cb18a01d2b75798efa729132e
Tony McCoy <tony@ratwizard.io>
2022-07-30 14:21:29 -0700

    Convert example project to modules, removed page rendering stuff

--- examples/server/app.js
+++ examples/server/app.js
@@ -1,4 +1,4 @@
-const { makeApp, makeRouter } = require("@woofjs/server");
+import { makeApp, makeRouter } from "@woofjs/server";
 
 const app = makeApp();
 
@@ -87,81 +87,6 @@ app.mount("/router", router);
 app.get("/hello", (ctx) => {
   return "Hello world.";
 });
-
-// app.get("/html", async (ctx) => {
-//   return (
-//     <main>
-//       <AsyncHeader />
-//       <p>This is an HTML page.</p>
-//     </main>
-//   );
-// });
-
-// async function AsyncHeader() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(
-//         <header>
-//           <h1>HELLO!</h1>
-//         </header>
-//       );
-//     }, 100);
-//   });
-// }
-
-// app.get("/component", () => {
-//   return <Component />;
-// });
-
-// app.get("/form", () => {
-//   return (
-//     <Page title="Form Test">
-//       <Form />
-//     </Page>
-//   );
-// });
-
-// app.get("/form/submitted", () => {
-//   return (
-//     <Page title="Thanks">
-//       <main>
-//         <h1>Thank you!</h1>
-//         <p>Your form has been submitted.</p>
-//         <p>
-//           <a href="/form">Return to Form</a>
-//         </p>
-//       </main>
-//     </Page>
-//   );
-// });
-
-// app.post("/form", (ctx) => {
-//   console.log("body", ctx.request.body);
-
-//   ctx.redirect("/form/submitted", 303);
-// });
-
-// function Page() {
-//   return (
-//     <html>
-//       <head>
-//         <meta charset="utf-8" />
-//         <title>{this.attrs.title}</title>
-//       </head>
-//       <body>{this.children}</body>
-//     </html>
-//   );
-// }
-
-// function Form() {
-//   return (
-//     <form method="post" action="/form" enctype="multipart/form-data">
-//       <input name="image" type="file" accept="image/*" required multiple />
-//       <input name="description" type="text" placeholder="Description text." />
-//       <button>Submit Form</button>
-//     </form>
-//   );
-// }
 
 // Listen for HTTP requests on localhost at specified port number.
 app.listen(4000).then((info) => {
