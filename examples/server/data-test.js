a722ca8a579a7580c0a34144b8fe45970c80100d
Tony McCoy <tony@ratwizard.io>
2022-08-01 22:59:44 -0700

    Added data package to examples project

---
+++ examples/server/data-test.js
@@ -0,0 +1,18 @@
+import { makeModel, collectionOf } from "@woofjs/data";
+
+const Post = makeModel({
+  key: "id",
+  schema: (v) => {
+    return v
+      .object({
+        id: v.number(),
+      })
+      .strict();
+  },
+});
+
+const post = new Post({ id: 1 });
+
+const subscription = post.subscribe((value) => {});
+
+subscription.unsubscribe();
